e41299acdec7f04040dcad1150179f10f70add15
tianrking <tian.r.king@gmail.com>
2022-02-07 20:09:59 +0800

    fix dir

--- fastapi/Juzibot_speech_to_text/app/fast_test.py
+++ fastapi/Juzibot_speech_to_text/app/fast_test.py
@@ -12,7 +12,7 @@ async def audio_help():
     return "send_audio"
 
 class Item_audio(BaseModel):
-    flag: Optional[str] = None
+    lol: Optional[str] = None
     audio_name: Optional[str] = None
     audio_data: Optional[str] = None
 
@@ -22,12 +22,17 @@ async def create_item(item: Item_audio):
     gg=item.audio_data.replace("data:audio/silk;base64,","")
     print(gg)
     audio_data = base64.b64decode(str(gg))
-    fout = open(item.audio_name+".silk",'wb')
+    # fout = open(item.audio_name+".silk",'wb')
+    print("AA\n")
+    dir = "/code/"+item.audio_name+".silk"
+    fout = open(dir,'wb')
     fout.write(audio_data)
     fout.close()
-    os.system("bash ./converter.sh "+item.audio_name+".silk"+" mid.mp3")
+    print("BB\n")
+    os.system("bash ./converter.sh "+dir+" mid.mp3")
     
-    song = AudioSegment.from_mp3(item.audio_name+".silk.mid.mp3")
+    print("CC\n")
+    song = AudioSegment.from_mp3(dir+".mid.mp3")
     song.export(item.audio_name+".wav", format="wav")
 
     import paddle
